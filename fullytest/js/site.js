e2af1c9f23c603239b434a466e6c529d1df74cc4
Wayne Hall <waynehall@Waynes-MacBook-Pro.local>
2017-02-08 12:26:00 -0800

    blah

--- fullytest/js/site.js
+++ fullytest/js/site.js
@@ -1,9 +1,39 @@
 var app = angular.module('app', ['ngAnimate']);
 
 
-app.controller('AppController', function ($scope){
-  $scope.msg = "Don't give up!";
+app.controller('AppController', ['$scope', '$http', '$q', function($scope, $http,$q) { 
+  console.log("hi")
+  $http.get('https://spreadsheets.google.com/feeds/list/1GY-HS0jdJD6Vsj3W7yUSjcoouYjIo90OuMS0-GQ6Qzo/1/public/values?alt=json').then(function (success){ 
+    console.log(success.data);
+    $scope.products = success.data.feed.entry;  console.log("Hey I worked!");
+  var data = eval($scope.products);
+  $scope.backorders = []
+  $scope.outofstocks = []
+  console.log("printing products" + $scope.products)
+
+  for (product in $scope.products){
+    console.log("Hey i'm working this loop for backorders- " + Object.values($scope.products[product].gsx$product));
+    if(Object.values($scope.products[product].gsx$type) == "Backorder"){
+      $scope.backorders.push($scope.products[product])
+    } 
+  }
   
+  for (product in $scope.products){
+    console.log("Hey i'm working this loop for Out of Stock- " + Object.values($scope.products[product].gsx$product));
+    if(Object.values($scope.products[product].gsx$type) == "Out of Stock"){
+      $scope.outofstocks.push($scope.products[product])
+    } 
+  }
 
+  for(backorder in $scope.backorders){
+    console.log("printing backorders - " + Object.values($scope.backorders[backorder].gsx$product))
+  }
+  for(oos in $scope.outofstocks){
+    console.log("printing out of stocks - " + Object.values($scope.outofstocks[oos].gsx$product))
+  }
 
+  }, function(error){
+    console.log("woops")
   })
+ 
+}]);
