ac001a3b9e473401badb670d1ca9744d351f5f57
Wayne Hall <wayneculverhall@gmail.com>
2018-04-25 09:54:47 -0700

    Add files via upload

--- fullytest/js/sig.js
+++ fullytest/js/sig.js
@@ -3,9 +3,11 @@ var app = angular.module('app', ['ngAnimate']);
 console.log("hi")
 app.controller('AppController', ['$scope', '$http', '$q', function($scope, $http,$q) { 
   console.log("hi")
+  $scope.number2type = ""
   $scope.name = "Webgility Guy"
   $scope.title = "Stafford's Nightmare"
   $scope.number = "503-503-5003"
- 
+  $scope.number2 = ""
+  $scope.pronouns = ""
  
 }]);
